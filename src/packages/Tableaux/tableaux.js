b12976a8d0c05cad0614ceef6627b6b10c7b2d74
Gabriel <gabrielbrazcs@gmail.com>
2020-07-13 01:21:49 -0300

    conclui o algoritmo que cria o tableaux

--- src/packages/Tableaux/tableaux.js
+++ src/packages/Tableaux/tableaux.js
@@ -1,32 +1,107 @@
 import Branch from './Branch';
+import Branches from './Branches';
 import branchResult from './utils';
 
-const stack = [];
+const stack = new Branches();
 
 class Tableaux {
 	constructor(formule, value) {
 		const branch = new Branch();
+		const stack =  new Branches();
+
 		const node = new Node(formule, value);
 		branch.push(node);
-		stack.push(branch);
+
+		stack.insert(branch);
 	
-		this.tableaux = new Tree(formule, value);
+		this.tableaux = new Tree(node);
 		this.tableaux.create();
 	}
 }
 
 class Tree {
-	constructor(formule, value) {
-		this.node = new Node(formule, value);
+	constructor(node) {
+		this.node = node;
 
 		this.left = null;
 		this.right = null;
 
-		// create();
+		this.create();
 	}
 
 	create() {
+		if (this.right) {
+			this.right.create();
+			return;
+		}
 
+		const branch = stack.get();
+
+		if (branch.empty()) {
+			stack.remove();
+			return;
+		}
+
+		const node = branch.pop();
+
+		if (node.formule.isAtom()) {
+			const { error } = branch.validate(node.formule.right, node.value);
+
+			if (error) {
+				stack.remove();
+				return;
+			}else {
+				const right_node = new Node(node.formule.right, node.value);
+				this.right = new Tree(right_node);
+				this.right.create();
+				return;
+			}
+		}else {
+			const { branch: split_branch, left, right } = node.willBranch();
+
+			if (!split_branch) {
+				if (node.formule.isOpUnary()) {
+					const right_node = new Node(node.formule.right, right);
+					branch.push(right_node);
+
+					this.right = new Tree(right_node);
+					
+					this.right.create();
+					return;
+				}else {
+					const left_node = new Node(node.formule.left, left);
+					const right_node = new Node(node.formule.right, right);
+
+					branch.push(left_node);
+					branch.push(right_node);
+
+					this.right = new Tree(left_node);
+					this.right.right = new Tree(right_node);
+
+					this.right.create();
+					return;
+				}
+			}else {
+				const copied_branch = branch.copy();
+
+				const left_node = new Node(node.formule.left, left);
+				const right_node = new Node(node.formule.right, right);
+
+				copied_branch.push(left_node);
+				branch.push(right_node);
+
+				stack.insert(copied_branch);
+
+				this.left = new Tree(left_node);
+				this.right = new Tree(right_node);
+
+				this.left.create();
+				stack.remove();
+
+				this.right.create();
+				return;
+			}
+		}
 	}
 }
 
@@ -40,9 +115,10 @@ class Node {
 		const operator = this.formule.operator;
 		const value = this.value === 1 ? 'positive' : 'negative';
 		const result = branchResult[operator][value];
-		
+
 		return result;
 	}
 }
 
 export { Node };
+export default Tableaux;
