0effdb3c838f9af12c4f9c0bba0b581c76274039
Vitor Maia <vlma@cin.ufpe.br>
2020-07-31 20:07:48 -0300

    remove uneeded import

--- src/components/navigation-bar/index.js
+++ src/components/navigation-bar/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Link from './link';
 import { useLocation } from 'react-router-dom';
 import './styles/navigation-bar.css';
